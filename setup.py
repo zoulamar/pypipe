f95603e06615bc0ae0db85b09361f8e5981c1c47
zoulamar@mzws <zoulamar@mzws>
2023-08-31 16:11:02 +0200

    Try to add command line utility.

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
-setup(name='pypipe',
-      version='0.1',
-      description='Automated and efficient evaluation framework.',
-      author='zoulamar',
-      author_email='zoulamar@fel.cvut.cz',
-      license='MIT',
-      packages=['pypipe'],
-      zip_safe=False)
+setup(
+    name='pypipe',
+    version='0.1',
+    description='Automated and efficient evaluation framework.',
+    author='zoulamar',
+    author_email='zoulamar@fel.cvut.cz',
+    license='MIT',
+    packages=['pypipe'],
+    scripts=['bin/pypipe'],
+    zip_safe=False,
+)
