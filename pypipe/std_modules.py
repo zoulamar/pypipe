78d830b573e70a76c2adcdbb82485663b50c76c8
zoulamar@cras-01 <zoulamar@cras-01>
2024-05-09 13:05:06 +0200

    Draft std_modules.RemoteData

--- pypipe/std_modules.py
+++ pypipe/std_modules.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from pypipe.sciplotrc import subplots, IEEE_COL_WIDTH
 import os
+import shutil
 
 class RootModule (BaseModule):
     """ Just a generic class which blocks further recursive submodule search. """
@@ -64,3 +65,55 @@ class Plot (BaseModule):
         except:
             os.remove(target.path)
             raise
+
+class RemoteData (RootModule):
+    def __init__(self, module_path: Path, source_space: Path, verbose: bool) -> None:
+        super().__init__(module_path, source_space, verbose)
+
+    def declare_targets(self) -> dict[str, GenericDataType]:
+        config = YamlDataType(self.module_path / "config.yaml", GenericDataType.expect_made)
+        ret = {}
+
+        # This represents a meta-target which factually copies the remote files.
+        targets = YamlDataType(self.module_path / "targets.yaml", self.refresh_targets, depends={"config": config})
+        ret["__targets__"] = targets
+
+        # If the target is already made, which must be done previously, a list of main targets may be declared.
+        if targets.path.exists():
+            for datafile_path in map(Path, targets.get()):
+                ret[datafile_path.stem] = GenericDataType(datafile_path, GenericDataType.expect_made, {})
+        return ret
+
+    def refresh_targets (self, target:YamlDataType):
+        """ Finds, copies and links the targets. """
+        config = target.depends["config"].get()
+
+        # Detect all remote files to be copied / cached. No problem if
+        globs = [config["path"]] if "path" in config else config["paths"]
+        assert isinstance(globs, list)
+        missing_is_ok = config["mounted"]
+        paths = []
+        for glob in globs:
+            if glob.startswith("/"):
+                glob = glob[1:]
+            result = list(Path("/").glob(glob))
+            if len(result) == 0 and not missing_is_ok:
+                raise FileNotFoundError(glob)
+            for match in result:
+                if match.exists():
+                    paths.append(match)
+                elif not missing_is_ok:
+                    raise FileNotFoundError(glob)
+
+        target_dir = target.path.parent
+        result_paths = []
+        touch_paths = []
+        for src_path in paths:
+            assert isinstance(src_path, Path)
+            dst_path = target_dir / src_path.name
+            touch_paths.append(dst_path)
+            result_paths.append(str(shutil.copy2(src_path, dst_path)))
+        target.set(result_paths)
+
+        for dst_path in touch_paths:
+            dst_path.touch()
