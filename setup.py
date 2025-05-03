907535be32093a3f3fe308018e03e533d834a6bc
Martin Zoula <zoulamar@fel.cvut.cz,zoulamar@seznam.cz>
2025-05-03 22:50:17 +0200

    Update dependencies.

--- setup.py
+++ setup.py
@@ -4,10 +4,16 @@ setup(
     name='pypipe',
     version='0.1',
     description='Automated evaluation framework.',
-    author='zoulamar',
+    author='Martin Zoula',
     author_email='zoulamar@fel.cvut.cz',
     license='MIT',
-    packages=['pypipe'],
+    packages=['pypipe', 'pypipe.modules', 'pypipe.datatypes'],
     scripts=['bin/pypipe'],
     zip_safe=False,
+    install_requires=[
+       'colored',
+       'pyyaml',
+       'numpy',
+    ]
 )
+
